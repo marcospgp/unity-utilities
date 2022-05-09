625b7f48d182c6539904bd2d9b542ceda051c2ad
Marcos Pereira <marcospereiradev@gmail.com>
2022-05-09 19:31:29 +0100

    Update Vector3Double.cs

--- Vector3Double.cs
+++ Vector3Double.cs
@@ -6,14 +6,19 @@ namespace MarcosPereira.UnityUtilities {
         public readonly double y;
         public readonly double z;
 
-        public static Vector3Double zero => new Vector3Double(0, 0, 0);
+        public Vector3Double(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
 
-        public static Vector3Double Lerp(
-            Vector3Double a,
-            Vector3Double b,
-            float t
-        ) =>
-            a + ((b - a) * t);
+        public static Vector3Double Zero => new Vector3Double(0, 0, 0);
+
+        public static explicit operator Vector3Double(Vector3Int a) =>
+            new Vector3Double(a.x, a.y, a.z);
+
+        public static explicit operator Vector3(Vector3Double a) =>
+            new Vector3((float) a.x, (float) a.y, (float) a.z);
 
         public static Vector3Double operator -(
             Vector3Double a,
@@ -45,16 +50,11 @@ namespace MarcosPereira.UnityUtilities {
         ) =>
             new Vector3Double(a.x * b, a.y * b, a.z * b);
 
-        public static explicit operator Vector3Double(Vector3Int a) =>
-            new Vector3Double(a.x, a.y, a.z);
-
-        public static explicit operator Vector3(Vector3Double a) =>
-            new Vector3((float) a.x, (float) a.y, (float) a.z);
-
-        public Vector3Double(double x, double y, double z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
+        public static Vector3Double Lerp(
+            Vector3Double a,
+            Vector3Double b,
+            float t
+        ) =>
+            a + ((b - a) * t);
     }
 }
