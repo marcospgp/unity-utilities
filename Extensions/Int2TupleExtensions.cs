f37324a51d70b2c78f114d95b2fa6eb54eed6649
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2022-10-24 23:39:41 +0100

    Update Int2TupleExtensions.cs

--- Extensions/Int2TupleExtensions.cs
+++ Extensions/Int2TupleExtensions.cs
@@ -4,5 +4,8 @@ namespace MarcosPereira.UnityUtilities {
     public static class Int2TupleExtensions {
         public static float Norm(this (int x, int z) t) =>
             Mathf.Sqrt(Mathf.Pow(t.x, 2f) + Mathf.Pow(t.z, 2f));
+
+        public static (int, int) Add(this (int a, int b) i, (int a, int b) j) =>
+            (i.a + j.a, i.b + j.b);
     }
 }
