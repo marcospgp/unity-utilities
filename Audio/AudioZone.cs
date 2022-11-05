b4283f2050782462e93ebaa8834717f72afba34a
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2022-11-05 12:15:57 +0000

    Organize

---
+++ Audio/AudioZone.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using UnityEngine;
+
+// Plays audio based on a collider. Different modes available.
+// Requires having a collider on the same GameObject as this.
+// Does not require an audio source - this component adds its own.
+
+namespace MarcosPereira.UnityUtilities {
+    [RequireComponent(typeof(Collider))]
+    [DisallowMultipleComponent]
+    public class AudioZone : MonoBehaviour {
+        [SerializeField, TagSelect]
+        [Tooltip("The tag of the collider that should trigger the sound from this component.")]
+        private string colliderTag;
+
+        [SerializeField]
+        private AudioClip audioClip;
+
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float volume = 0.5f;
+
+        [SerializeField]
+        private Mode mode = Mode.PlayWhileInside;
+
+        [Header("PlayWhileInside")]
+
+        [SerializeField]
+        [Range(0f, 5f)]
+        [Tooltip("How much the volume can change per second.")]
+        private float volumeFadeSpeed = 2f;
+
+        [Header("PlayOnEnter/PlayOnExit")]
+
+        [SerializeField]
+        [Tooltip(
+            "Set whether the player's speed is taken into account when determining the audio " +
+            "volume.\nEnabling this requires there to be a character controller in " +
+            "one of the player camera's parent gameObjects."
+        )]
+        private bool scaleVolumeByPlayerSpeed;
+
+        [SerializeField]
+        [Tooltip(
+            "Use this to determine which components of the player's movement " +
+            "cause the volume to increase. For example, a vertical water " +
+            "splash can be set with (0, -1, 0)")]
+        private Vector3 velocityMultiplier = Vector3.one;
+
+        [SerializeField]
+        [Tooltip(
+            "The player speed upon entry/exit at which the sound is played at full volume " +
+            "(full volume is defined above)."
+        )]
+        private float maxVolumeSpeed = 10f;
+
+        private AudioSource audioSource;
+
+        private Coroutine volumeCoroutine;
+
+        private CharacterController charController;
+
+        private enum Mode {
+            PlayWhileInside,
+            PlayOnEnter,
+            PlayOnExit
+        }
+
+        public void Awake() {
+            if (!this.GetComponent<Collider>().isTrigger) {
+                throw new System.Exception("Trigger collider required.");
+            }
+
+            this.audioSource = this.gameObject.AddComponent<AudioSource>();
+            this.audioSource.spatialBlend = 0f; // 2D sound
+            this.audioSource.volume = 0f;
+            this.audioSource.playOnAwake = false;
+
+            if (this.scaleVolumeByPlayerSpeed) {
+                this.charController = GameObject
+                    .FindGameObjectWithTag(this.colliderTag)
+                    .GetComponentInParent<CharacterController>();
+
+                if (this.charController == null) {
+                    throw new System.Exception(
+                        "Missing character controller in player camera's parents."
+                    );
+                }
+            }
+        }
+
+        public void OnTriggerEnter(Collider other) {
+            if (!other.CompareTag(this.colliderTag)) {
+                return;
+            }
+
+            if (this.mode == Mode.PlayWhileInside) {
+                this.audioSource.clip = this.audioClip;
+                this.audioSource.loop = true;
+
+                this.volumeCoroutine =
+                    this.StartCoroutine(this.FadeVolume(this.volume));
+            } else if (this.mode == Mode.PlayOnEnter) {
+                this.audioSource.volume = this.GetVolume();
+                this.audioSource.PlayOneShot(this.audioClip);
+            }
+        }
+
+        public void OnTriggerExit(Collider other) {
+            if (!other.CompareTag(this.colliderTag)) {
+                return;
+            }
+
+            if (this.mode == Mode.PlayWhileInside) {
+                this.volumeCoroutine =
+                    this.StartCoroutine(this.FadeVolume(0f));
+            } else if (this.mode == Mode.PlayOnExit) {
+                this.audioSource.volume = this.GetVolume();
+                this.audioSource.PlayOneShot(this.audioClip);
+            }
+        }
+
+        private IEnumerator FadeVolume(float targetVolume) {
+            if (this.volumeCoroutine != null) {
+                this.StopCoroutine(this.volumeCoroutine);
+            }
+
+            if (targetVolume > 0f) {
+                this.audioSource.Play();
+            }
+
+            float volume01 = Mathf.Clamp01(targetVolume);
+
+            while (this.audioSource.volume != volume01) {
+                this.audioSource.volume =
+                    Mathf.MoveTowards(
+                        this.audioSource.volume,
+                        volume01,
+                        this.volumeFadeSpeed * Time.deltaTime
+                    );
+
+                yield return null;
+            }
+
+            if (this.audioSource.volume == 0f) {
+                this.audioSource.Stop();
+            }
+        }
+
+        private float GetVolume() {
+            if (!this.scaleVolumeByPlayerSpeed) {
+                return this.volume;
+            }
+
+            float speed = Vector3.Magnitude(
+                Vector3.Scale(
+                    this.charController.velocity, this.velocityMultiplier
+                )
+            );
+
+            float ratio = Mathf.Clamp01(speed / this.maxVolumeSpeed);
+
+            return ratio * this.volume;
+        }
+    }
+}
---
+++ Effects/FogZone.cs
@@ -0,0 +1,102 @@
+using MarcosPereira.UnityUtilities;
+using UnityEngine;
+
+namespace MarcosPereira {
+    [RequireComponent(typeof(Collider))]
+    public class FogZone : MonoBehaviour {
+        [SerializeField]
+        [TagSelect]
+        private string playerCameraTag;
+
+        [SerializeField]
+        [Tooltip(
+            "Optionally hide some objects when the fog kicks in, such as " +
+            "hiding the water surface when entering underwater mode."
+        )]
+        private Renderer[] objectsToHide;
+
+        [SerializeField]
+        private Color color;
+
+        [SerializeField]
+        private float density;
+
+        [SerializeField]
+        private FogMode mode;
+
+        private FogSettings previousFog;
+
+        private bool[] objectsHidden;
+
+        public void OnTriggerEnter(Collider other) {
+            if (other.CompareTag(this.playerCameraTag)) {
+                this.OnUnderwater();
+            }
+        }
+
+        public void OnTriggerExit(Collider other) {
+            if (other.CompareTag(this.playerCameraTag)) {
+                this.OnOverWater();
+            }
+        }
+
+        private void OnUnderwater() {
+            this.objectsHidden = new bool[this.objectsToHide.Length];
+
+            for (int i = 0; i < this.objectsToHide.Length; i++) {
+                var renderer = this.objectsToHide[i];
+
+                if (renderer.enabled) {
+                    renderer.enabled = false;
+                    this.objectsHidden[i] = true;
+                }
+            }
+
+            this.previousFog = new FogSettings() {
+                enabled = RenderSettings.fog,
+                color = RenderSettings.fogColor,
+                density = RenderSettings.fogDensity,
+                mode = RenderSettings.fogMode,
+                startDistance = RenderSettings.fogStartDistance,
+                endDistance = RenderSettings.fogEndDistance
+            };
+
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = this.color;
+            RenderSettings.fogDensity = this.density;
+            RenderSettings.fogMode = this.mode;
+        }
+
+        private void OnOverWater() {
+            for (int i = 0; i < this.objectsToHide.Length; i++) {
+                var renderer = this.objectsToHide[i];
+
+                if (this.objectsHidden[i]) {
+                    renderer.enabled = true;
+                }
+            }
+
+            this.objectsHidden = null;
+
+            this.previousFog.Restore();
+        }
+
+        private struct FogSettings {
+            public bool enabled;
+            public Color color;
+            public float density;
+            public FogMode mode;
+            public float startDistance;
+            public float endDistance;
+
+            public void Restore() {
+                RenderSettings.fog = this.enabled;
+                RenderSettings.fogColor = this.color;
+                RenderSettings.fogDensity = this.density;
+                RenderSettings.fogMode = this.mode;
+                RenderSettings.fogStartDistance = this.startDistance;
+                RenderSettings.fogEndDistance = this.endDistance;
+            }
+        }
+    }
+}
