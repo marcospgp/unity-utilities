94789a9273c60c7db91ffa6e24d48467d34f5179
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2022-11-08 17:46:38 +0000

    Update Impact.cs

--- Effects/Impact.cs
+++ Effects/Impact.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -12,16 +10,31 @@ namespace MarcosPereira.UnityUtilities {
     [RequireComponent(typeof(Collider))]
     public class Impact : MonoBehaviour {
         [SerializeField]
+        [Tooltip("The layers that will trigger this component upon collision.")]
+        private LayerMask targetLayers;
+
+        [SerializeField]
         [Range(0f, 1f)]
         private float maxVolume = 0.5f;
 
         [SerializeField]
-        [Range(0f, 10f)]
         [Tooltip(
-            "The collision relative speed at which the sound is played at the max volume " +
+            "The relative collision speed at which the sound is played at the max volume " +
             "(defined above)."
         )]
         private float maxVolumeSpeed = 10f;
+
+        [SerializeField]
+        [Tooltip("The minimum relative collision speed for an impact to be triggered.")]
+        private float minSpeed;
+
+        [SerializeField]
+        [Tooltip(
+            "Use this to determine which components of the collision's relative velocity " +
+            "are taken into account when determining its relative speed.\n" +
+            "For example, a vertical water splash can be set with (0, 1, 0)."
+        )]
+        private Vector3 relativeVelocityMultiplier = Vector3.one;
 
         [SerializeField]
         private AudioMixerGroup audioMixer;
@@ -36,37 +49,85 @@ namespace MarcosPereira.UnityUtilities {
 
         private AudioSource audioSource;
 
+        private CharacterController charController;
+        private new Rigidbody rigidbody;
+
         public void Awake() {
-            this.audioSource = this.gameObject.AddComponent<AudioSource>();
+            var child = new GameObject("Audio Source");
+            child.transform.SetParent(this.transform);
+
+            this.audioSource = child.AddComponent<AudioSource>();
             this.audioSource.spatialBlend = 1f; // 3D sound
             this.audioSource.volume = 0f;
             this.audioSource.playOnAwake = false;
             this.audioSource.outputAudioMixerGroup = this.audioMixer;
+
+            this.charController = this.GetComponent<CharacterController>();
+            this.rigidbody = this.GetComponent<Rigidbody>();
         }
 
         public void OnCollisionEnter(Collision collision) {
-            float relativeSpeed = collision.relativeVelocity.magnitude;
+            if (IsLayerInMask(collision.gameObject.layer, this.targetLayers)) {
+                var contact = collision.GetContact(0);
+                var scaledVelocity =
+                    Vector3.Scale(collision.relativeVelocity, this.relativeVelocityMultiplier);
 
-            float ratio = Mathf.Clamp01(relativeSpeed / this.maxVolumeSpeed);
-
-            float volume = ratio * this.maxVolume;
-
-            // TODO: continue
-
-            UnityEngine.Debug.Log(
-                $"Impact OnCollisionEnter: {collision.collider.name}"
-            );
+                this.OnImpact(contact.point, contact.normal, scaledVelocity.magnitude);
+            }
         }
 
         public void OnTriggerEnter(Collider other) {
-            UnityEngine.Debug.Log(
-                $"Impact OnTriggerEnter: {other.name}"
-            );
+            if (IsLayerInMask(other.gameObject.layer, this.targetLayers)) {
+                Vector3 velocity = Vector3.zero;
+
+                if (other.TryGetComponent(out Rigidbody otherRigidbody)) {
+                    // Negative because we want relative velocity from this object to the other.
+                    velocity -= otherRigidbody.velocity;
+                }
+
+                if (this.charController != null) {
+                    velocity += this.charController.velocity;
+                } else {
+                    velocity += this.rigidbody.velocity;
+                }
+
+                this.OnImpact(this.transform.position, this.transform.up, velocity.magnitude);
+            }
         }
 
-        private void PlayRandomClip(AudioClip[] clips, float volume) {
-            AudioClip clip = clips[Random.Range(0, clips.Length)];
+        private static bool IsLayerInMask(int layer, int layerMask) =>
+            ((1 << layer) & layerMask) != 0;
+
+        private void OnImpact(Vector3 position, Vector3 normal, float speed) {
+            if (speed < this.minSpeed) {
+                return;
+            }
+
+            this.PlayRandomClip(position, speed);
+
+            foreach (var x in this.particleSystems) {
+                x.Stop();
+                x.transform.SetPositionAndRotation(
+                    position,
+                    x.transform.rotation * Quaternion.FromToRotation(x.transform.up, normal)
+                );
+                x.Play();
+            }
+        }
+
+        private void PlayRandomClip(Vector3 position, float speed) {
+            float volume = this.GetVolume(speed);
+
+            AudioClip clip = this.audioClips[Random.Range(0, this.audioClips.Length)];
+
+            this.audioSource.transform.position = position;
             this.audioSource.PlayOneShot(clip, volume);
+        }
+
+        private float GetVolume(float speed) {
+            float ratio = Mathf.Clamp01(speed / this.maxVolumeSpeed);
+
+            return Mathf.Pow(ratio, 2f) * this.maxVolume;
         }
     }
 }
