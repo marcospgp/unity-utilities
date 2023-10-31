4c36f19e28fea98471689b58392af43f4cbd0b08
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2023-10-31 15:57:42 +0000

    Update MathUtils.cs

--- MathUtils.cs
+++ MathUtils.cs
@@ -19,6 +19,16 @@ namespace UnityUtilities
         /// For more info, see: https://marcospereira.me/2022/08/24/lerp-how-to-frame-rate-independent
         /// </summary>
         /// <param name="d">How much a approaches b per second.</param>
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float d)
+        {
+            return Vector2.Lerp(b, a, Mathf.Pow(1f - d, Time.deltaTime));
+        }
+
+        /// <summary>
+        /// Frame rate independent lerp.
+        /// For more info, see: https://marcospereira.me/2022/08/24/lerp-how-to-frame-rate-independent
+        /// </summary>
+        /// <param name="d">How much a approaches b per second.</param>
         public static float Lerp(float a, float b, float d)
         {
             return Mathf.Lerp(b, a, Mathf.Pow(1f - d, Time.deltaTime));
